6b8afce8f4577ae9b12db8a7aad6f151dcb52dab
david benzaquen <david654100@gmail.com>
2021-01-19 12:35:55 -0500

    finnished problem 20

---
+++ src/main/java/problems/problem20.java
@@ -0,0 +1,20 @@
+package problems;
+import com.google.common.math.BigIntegerMath;
+
+import java.math.BigInteger;
+
+public class problem20 {
+
+
+    public static void main(String[] args) {
+       BigInteger big =BigIntegerMath.factorial(100);
+        String digits = big.toString();
+        int sum = 0;
+
+        for(int i = 0; i < digits.length(); i++) {
+            int digit = (int) (digits.charAt(i) - '0');
+            sum = sum + digit;
+        }
+        System.out.println(sum);
+    }
+}
