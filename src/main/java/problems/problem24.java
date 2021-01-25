bb98906cb84f78295c9fab6f850041ae75cb2cd2
david benzaquen <david654100@gmail.com>
2021-01-25 13:41:32 -0500

    finished problem 24

---
+++ src/main/java/problems/problem24.java
@@ -0,0 +1,21 @@
+package problems;
+
+import com.google.common.collect.Collections2;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+
+
+import static com.google.common.collect.Collections2.orderedPermutations;
+
+public class problem24 {
+    public static void main(String[] args) {
+        ArrayList<Object> list = new ArrayList<>(orderedPermutations(Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9)));
+
+       System.out.println(list.get(999999));
+
+    }
+}
+//[2, 7, 8, 3, 9, 1, 5, 4, 6, 0]
